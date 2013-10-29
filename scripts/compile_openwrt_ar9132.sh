377038ec719035fd8505173b339cb1754f1a6208
panticz <mail@konczalski.de>
2013-10-29 22:50:30 +0100

    Fix compile_openwrt_ar9132.sh

--- scripts/compile_openwrt_ar9132.sh
+++ scripts/compile_openwrt_ar9132.sh
@@ -1,12 +1,18 @@
 #/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y subversion build-essential subversion git-core libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl unzip
 
-su ubuntu -
+# install libs
+apt-get update
+apt-get install -y subversion build-essential subversion git-core libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl unzip
 
-mkdir /dev/shm/openwrt
-cd /dev/shm/openwrt
+
+# create OpenWrt toolchain
+useradd -m openwrt
+su openwrt
+
+mkdir ~/openwrt
+cd ~/openwrt
+
 svn co svn://svn.openwrt.org/openwrt/trunk/
 cd trunk
 
@@ -18,15 +24,17 @@ make defconfig
 make -j$(cat /proc/cpuinfo | grep processor | wc -l)
 #make prepare -j$(cat /proc/cpuinfo | grep processor | wc -l)
 
+
+# build lyc
 # export PATH=$PATH:/dev/shm/openwrt/trunk/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin
-export STAGING_DIR=/dev/shm/openwrt/trunk/staging_dir
+export STAGING_DIR=~/openwrt/trunk/staging_dir
 
 # get source
 wget https://raw.github.com/panticz/lyc/master/lyc.c -O lyc.c
 wget https://raw.github.com/panticz/lyc/master/rxvx700.h -O rxvx700.h
 
 #make CC=mips-openwrt-linux-uclibc-gcc LD=mips-openwrt-linux-uclibc-ld
-/dev/shm/openwrt/trunk/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-uclibc-gcc lyc.c -o lyc
+~/openwrt/trunk/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-uclibc-gcc lyc.c -o lyc
 
 # copy file to router
 scp ./lyc root@openwrt:/root
