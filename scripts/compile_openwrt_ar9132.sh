17ef6fca029face6d3757cde374b8cde7ccdc791
panticz <mail@konczalski.de>
2013-11-07 15:25:04 +0100

    Rewrite and cleanup

--- scripts/compile_openwrt_ar9132.sh
+++ scripts/compile_openwrt_ar9132.sh
@@ -1,33 +1,29 @@
-#/bin/bash
-
-
-# install libs
-apt-get update
+# installing required libraries 
 apt-get install -y subversion build-essential subversion git-core libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl unzip
 
-
-# create OpenWrt toolchain
+# create user for compile process
 useradd -m openwrt
+
+# login as openwrt
 su openwrt
 
-mkdir ~/openwrt
-cd ~/openwrt
-
+# download OpenWrt
 svn co svn://svn.openwrt.org/openwrt/trunk/
 cd trunk
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# configure OpenWRT
 make defconfig
 
+# compile OpenWRT
 make -j$(cat /proc/cpuinfo | grep processor | wc -l)
-#make prepare -j$(cat /proc/cpuinfo | grep processor | wc -l)
 
-
-# build lyc
+# download lyc
 wget -q https://github.com/panticz/lyc/archive/master.zip -O /tmp/master.zip
 unzip -q /tmp/master.zip
+
+# compile lyc
 cd lyc-master
 make mips
 
